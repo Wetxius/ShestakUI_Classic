272edf93fb8570a31ab5d563fb1a69713a060ef7
hehooisbored <30204325+hehooisbored@users.noreply.github.com>
2025-07-09 22:08:13 -0500

    Additional Monk related updates
    Updated Stagger, GCD, and Harmony Orbs to support monk in Mists

--- ShestakUI/Libs/oUF/Elements/Stagger.lua
+++ ShestakUI/Libs/oUF/Elements/Stagger.lua
@@ -1,3 +1,5 @@
+local T, C, L = unpack(ShestakUI)
+
 if(select(2, UnitClass('player')) ~= 'MONK') then return end
 
 local _, ns = ...
@@ -102,12 +104,12 @@ local function Path(self, ...)
 end
 
 local function Visibility(self, event, unit)
-	if(SPEC_MONK_BREWMASTER ~= GetSpecialization() or UnitHasVehiclePlayerFrameUI('player')) then
+	if(SPEC_MONK_BREWMASTER ~= T.GetSpecialization() or UnitHasVehiclePlayerFrameUI('player')) then
 		if(self.Stagger:IsShown()) then
 			self.Stagger:Hide()
 			self:UnregisterEvent('UNIT_AURA', Path)
 		end
-		if SPEC_MONK_WINDWALKER ~= GetSpecialization() then
+		if SPEC_MONK_WINDWALKER ~= T.GetSpecialization() then
 			if self.Debuffs then self.Debuffs:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", 2, 5) end	-- ShestakUI
 		end
 	else
