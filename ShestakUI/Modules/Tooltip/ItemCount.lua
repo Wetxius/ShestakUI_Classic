c068875e587fc727f50d308fa8f51ba5ccc96fe1
hehooisbored <30204325+hehooisbored@users.noreply.github.com>
2025-08-05 18:08:38 -0500

    Fix ItemCount LUA error

--- ShestakUI/Modules/Tooltip/ItemCount.lua
+++ ShestakUI/Modules/Tooltip/ItemCount.lua
@@ -7,7 +7,7 @@ if C.tooltip.enable ~= true or C.tooltip.item_count ~= true then return end
 local function OnTooltipSetItem(self, data)
 	if self ~= GameTooltip or self:IsForbidden() then return end
 	local num
-	if T.Classic then
+	if T.Classic and not T.Mists then
 		local _, link = self:GetItem()
 		num = C_Item.GetItemCount(link, true)
 	else
@@ -19,7 +19,7 @@ local function OnTooltipSetItem(self, data)
 	end
 end
 
-if T.Classic then
+if T.Classic and not T.Mists then
 	GameTooltip:HookScript("OnTooltipSetItem", OnTooltipSetItem)
 else
 	TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Item, OnTooltipSetItem)
